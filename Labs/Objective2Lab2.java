2b30a3b0b1834c26d02573f7a530329810c0622f
cas-e <caseyk6.28@gmail.com>
2023-06-22 18:32:45 -0700

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,13 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		// data
+		String name = "Casey Kahn";
+		int age = 33;
+		String state = "California";
+
+		// display
+		System.out.print("Name: " + name + " ");
+		System.out.print("Age: " + age + " ");
+		System.out.print("State :" + state + "\n");
+	}
+}
