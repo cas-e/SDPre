b65c41ab112ea0d5e4c6385d1e86bb2a5b83c115
cas-e <caseyk6.28@gmail.com>
2023-06-23 15:10:28 -0700

    complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,15 @@
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+		int sum = 0;
+		int count = 0;
+
+		while (count < 20) {
+			count++;
+			sum += count;
+		}
+
+		System.out.println(sum);
+
+
+	}
+}
