ddb4479e5fac9af4d0418942ddfe5a4a4ec3a463
cas-e <caseyk6.28@gmail.com>
2023-06-22 18:41:10 -0700

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int yearBorn = 1989;
+		String monthBorn = "June";
+		boolean isWeekend = false;
+
+		System.out.println(yearBorn);
+		System.out.println(monthBorn);
+		System.out.println(isWeekend);
+	}
+}
